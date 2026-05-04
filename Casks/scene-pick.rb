a706f447282f7cf2d26261fff873ec14736859e5
lishouxian <li.shouxian@outlook.com>
2026-05-05 00:01:56 +0800

    Harden ScenePick cask install path

--- Casks/scene-pick.rb
+++ Casks/scene-pick.rb
@@ -1,14 +1,22 @@
 cask "scene-pick" do
-  version :latest
-  sha256 :no_check
+  version "1.0.0"
+  sha256 "d610eba6e8871455b9be016a04b45633ef3c1ed87b0748fe1aeb8f11976c6625"
 
-  url "https://github.com/lishouxian/ScenePick/releases/latest/download/ScenePick.dmg"
+  url "https://github.com/lishouxian/ScenePick/releases/download/v#{version}/ScenePick.dmg"
   name "ScenePick"
   name "拾景"
   desc "Menu bar app for browsing Bing daily wallpapers and setting the macOS desktop wallpaper"
   homepage "https://github.com/lishouxian/ScenePick"
 
+  depends_on macos: ">= :ventura"
+
   app "ScenePick.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/ScenePick.app"],
+                   sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/ScenePick",
